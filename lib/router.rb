3baba812192892b13eb2b04665c1e0a527ec0ec2
App Academy Student <appacademy@Apps-Mac-mini-260.local>
2016-01-06 17:33:53 -0500

    Write Route#run.

--- lib/router.rb
+++ lib/router.rb
@@ -16,8 +16,12 @@ class Route
   # use pattern to pull out route params (save for later?)
   # instantiate controller and call controller action
   def run(req, res)
-
+    new_controller = @controller_class.new
+    new_controller.invoke_action(req.request_method)
+    new_controller.invoke_action(match(req))
+    res.status = 404
   end
+  
 end
 
 class Router
@@ -29,8 +33,7 @@ class Router
 
   # simply adds a new route to the list of routes
   def add_route(pattern, method, controller_class, action_name)
-    @routes << Route.new(pattern, http_method, controller_class, action_name)
-    
+    @routes << Route.new(pattern, method, controller_class, action_name)
   end
 
   # evaluate the proc in the context of the instance
@@ -41,13 +44,21 @@ class Router
   # make each of these methods that
   # when called add route
   [:get, :post, :put, :delete].each do |http_method|
+    define_method(http_method) do |pattern, controller_class, action_name|
+      add_route(pattern, http_method, controller_class, action_name)
+    end
   end
 
   # should return the route that matches this request
   def match(req)
+    @routes.each do |route|
+      return route if route.matches?(req)
+    end
+    nil
   end
 
   # either throw 404 or call run on a matched route
   def run(req, res)
+
   end
 end
